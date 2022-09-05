d1edae65f281dd15dd9e8aa81fe6256c80ee6481
DEVudg22 <112898483+DEVudg22@users.noreply.github.com>
2022-09-05 14:02:31 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+   <h1 className="title">
+      Bienvenido al Sistema de Captura de Información Penitenciaria <span aria-label="emoji" role="img"></span>
+   </h1>
+   
+   <input type="text" className="new-task" placeholder="usuario"/>
+   <input type="password" className="new-task" placeholder="contraseña"/>
+  
+   <input type="submit" className="new-task" value="INGRESAR"/>
+</div>
+  );
+}
+
+export default App;
